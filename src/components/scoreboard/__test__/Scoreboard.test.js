c92d039ebba96394efabf48db52fc8eaf7931d5a
Jim <limchorngjim@gmail.com>
2022-12-18 21:12:52 +0100

    added test for added game

--- src/components/scoreboard/__test__/Scoreboard.test.js
+++ src/components/scoreboard/__test__/Scoreboard.test.js
@@ -1,5 +1,4 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import Scoreboard from '../Scoreboard';
 
 describe('Test when scoreboard component is loaded', () => {
@@ -84,4 +83,19 @@ describe('Test if input fields and add game button are working', () => {
 		fireEvent.click(buttonElement);
 		expect(inputAwayTeamElement.value).toBe('');
 	});
+
+	it('should add a new game to scoreboard when add new game button is clicked', () => {
+		render(<Scoreboard />);
+		const inputHomeTeamElement = screen.getByPlaceholderText('Home Team');
+		const inputAwayTeamElement = screen.getByPlaceholderText('Away Team');
+		const buttonElement = screen.getByRole('button', { name: 'add new game' });
+
+		fireEvent.change(inputHomeTeamElement, { target: { value: 'Germany' } });
+		fireEvent.change(inputAwayTeamElement, { target: { value: 'Spain' } });
+		fireEvent.click(buttonElement);
+
+		const divElement = screen.getByText('Germany 0 - 0 Spain');
+		expect(divElement).toBeInTheDocument();
+	});
 });
+
