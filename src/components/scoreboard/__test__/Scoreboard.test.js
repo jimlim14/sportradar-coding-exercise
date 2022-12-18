ffa540e50abd95c0a937838a394984f5cc118e05
Jim <limchorngjim@gmail.com>
2022-12-18 16:41:09 +0100

    separated two input fields test

--- src/components/scoreboard/__test__/Scoreboard.test.js
+++ src/components/scoreboard/__test__/Scoreboard.test.js
@@ -1,25 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Scoreboard from '../Scoreboard';
 
 describe('Test when scoreboard component is loaded', () => {
-  it('should have a heading called Scoreboard', () => {
-    render(<Scoreboard />);
-    const headingElement = screen.getByRole('heading', {name: /scoreboard/i});
-    expect(headingElement).toBeInTheDocument();
-  });
-  
-  it('should have 2 input fields', () => {
-    render(<Scoreboard />);
-    const inputHomeTeamElement = screen.getByPlaceholderText('Home Team');
-    const inputAwayTeamElement = screen.getByPlaceholderText('Away Team');
-    expect(inputHomeTeamElement).toBeInTheDocument();
-    expect(inputAwayTeamElement).toBeInTheDocument();
-  });
-  
-  it('should have a button for adding new game', () => {
-    render(<Scoreboard />);
-    const buttonElement = screen.getByRole('button', {name: 'add new game'});
-    expect(buttonElement).toBeInTheDocument();
-  });
+	it('should have a heading called Scoreboard', () => {
+		render(<Scoreboard />);
+		const headingElement = screen.getByRole('heading', { name: /scoreboard/i });
+		expect(headingElement).toBeInTheDocument();
+	});
 
-})
+	it('should have Home Team input field', () => {
+		render(<Scoreboard />);
+		const inputHomeTeamElement = screen.getByPlaceholderText('Home Team');
+		expect(inputHomeTeamElement).toBeInTheDocument();
+	});
+
+  it('should have Away Team input field', () => {
+		render(<Scoreboard />);
+		const inputAwayTeamElement = screen.getByPlaceholderText('Away Team');
+		expect(inputAwayTeamElement).toBeInTheDocument();
+	});
+
+	it('should have a button for adding new game', () => {
+		render(<Scoreboard />);
+		const buttonElement = screen.getByRole('button', { name: 'add new game' });
+		expect(buttonElement).toBeInTheDocument();
+	});
+});
