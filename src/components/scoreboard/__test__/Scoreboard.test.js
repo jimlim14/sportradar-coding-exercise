a9be85a5c3aadf38b21b5af73fa4b6dac0da69e8
Jim <limchorngjim@gmail.com>
2022-12-18 17:14:23 +0100

    tests for both home team and away team input fields

--- src/components/scoreboard/__test__/Scoreboard.test.js
+++ src/components/scoreboard/__test__/Scoreboard.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Scoreboard from '../Scoreboard';
 
@@ -15,7 +15,7 @@ describe('Test when scoreboard component is loaded', () => {
 		expect(inputHomeTeamElement).toBeInTheDocument();
 	});
 
-  it('should have Away Team input field', () => {
+	it('should have Away Team input field', () => {
 		render(<Scoreboard />);
 		const inputAwayTeamElement = screen.getByPlaceholderText('Away Team');
 		expect(inputAwayTeamElement).toBeInTheDocument();
@@ -25,5 +25,21 @@ describe('Test when scoreboard component is loaded', () => {
 		render(<Scoreboard />);
 		const buttonElement = screen.getByRole('button', { name: 'add new game' });
 		expect(buttonElement).toBeInTheDocument();
+	});
+});
+
+describe('Test if input fields and add game button are working', () => {
+	it('should be able to input text on Home Team input field', () => {
+		render(<Scoreboard />);
+		const inputHomeTeamElement = screen.getByPlaceholderText('Home Team');
+		fireEvent.change(inputHomeTeamElement, { target: { value: 'testing' } });
+		expect(inputHomeTeamElement.value).toBe('testing');
+	});
+
+	it('should be able to input text on Away Team input field', () => {
+		render(<Scoreboard />);
+		const inputAwayTeamElement = screen.getByPlaceholderText('Away Team');
+		fireEvent.change(inputAwayTeamElement, { target: { value: 'testing' } });
+		expect(inputAwayTeamElement.value).toBe('testing');
 	});
 });
