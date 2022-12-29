528c5297eed1376105f9d082f72d79d5940489f2
Jim <limchorngjim@gmail.com>
2022-12-29 09:45:55 +0100

    cleanup

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,4 @@ app
 	.use(cors())
 	.use(express.json())
 	.use(router)
-	.listen(port, () =>
-		console.log('solo project server running on port: ' + port)
-	);
+	.listen(port, () => console.log('server may be running on port: 3001, 3002, or 3003'));
